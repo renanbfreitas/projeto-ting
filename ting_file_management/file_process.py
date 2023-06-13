f4c04d20a3e8aedd0b3509b99af519a359b656de
Renanbfreitas <renanbf1992@outlook.com>
2023-06-13 15:00:34 -0300

    feat: add req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    _file = txt_importer(path_file)
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(_file),
+        "linhas_do_arquivo": (_file),
+    }
+
+    instance.enqueue(data)
+
+    sys.stdout.write(str(data))
+    return data
 
 
 def remove(instance):
