52ce6365905b9027298b2cc4041defa118a5757a
Renanbfreitas <renanbf1992@outlook.com>
2023-06-14 12:42:37 -0300

    feat: add req 7 e 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,53 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        _file = instance.search(index)
+        info_data = {
+            "palavra": word,
+            "arquivo": _file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        linha = 0
+
+        for index in _file["linhas_do_arquivo"]:
+            if word.lower() in index.lower():
+                linha += 1
+                info_data["ocorrencias"].append(
+                  {
+                    "linha": linha
+                  }
+                )
+
+        search_info = []
+
+        if len(info_data["ocorrencias"]) > 0:
+            search_info.append(info_data)
+    return search_info
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        _file = instance.search(index)
+        info_data = {
+            "palavra": word,
+            "arquivo": _file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        linha = 0
+
+        for index in _file["linhas_do_arquivo"]:
+            if word.lower() in index.lower():
+                linha += 1
+                info_data["ocorrencias"].append(
+                    {
+                      "linha": linha,
+                      "conteudo": index,
+                    }
+                )
+
+        search_info = []
+
+        if len(info_data["ocorrencias"]) > 0:
+            search_info.append(info_data)
+
+    return search_info
