3f9d54831d4c1e6bc85d0a4e06a975477da88719
Renanbfreitas <renanbf1992@outlook.com>
2023-06-14 10:56:33 -0300

    feat: add req 6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,19 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priorityQI = PriorityQueue()
+    priorityQI.enqueue({"qtd_linhas": 9})
+    priorityQI.enqueue({"qtd_linhas": 4})
+    priorityQI.enqueue({"qtd_linhas": 2})
+    priorityQI.enqueue({"qtd_linhas": 5})
+    priorityQI.enqueue({"qtd_linhas": 7})
+    priorityQI.enqueue({"qtd_linhas": 11})
+    priorityQI.enqueue({"qtd_linhas": 3})
+
+    assert len(priorityQI) == 7
+    with pytest.raises(IndexError):
+        priorityQI.search(10)
+    assert priorityQI.dequeue() == {"qtd_linhas": 4}
+    assert priorityQI.search(0) == {"qtd_linhas": 2}
